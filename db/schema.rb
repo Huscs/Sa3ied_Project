603c23e6f470c6dd522441173ef35e755f5c2de7
Hussam <huscs@yahoo.com>
2015-11-27 21:16:26 +0300

    Add user.rb and User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127174759) do
+ActiveRecord::Schema.define(version: 20151127180718) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
