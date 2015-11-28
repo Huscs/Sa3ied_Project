e831b7836c665a5a27b69ecd60904fcb2cc1bd28
Hussam <huscs@yahoo.com>
2015-11-29 01:06:33 +0300

    Add: Update, edit and show methods in UsersController, create a new views: show & edit users .. Finally: add a gravatar in application_helper and user it as a profile img

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,22 @@ class UsersController < ApplicationController
 		
 	end
 
+	def edit
+
+		@user =  User.find(params[:id])
+		
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+		flash[:success] = "تم تحديث بياناتك بنجاح"
+		redirect_to root_path
+		else
+		render 'edit'
+		end
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password) #create a new user object
