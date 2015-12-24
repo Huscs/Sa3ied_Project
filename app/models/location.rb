f2cff2fa51aa27eef6a161c39f88ee1473ee5d66
Hussam <huscs@yahoo.com>
2015-12-24 16:39:47 +0300

    Add scenario: A help seeker fill in wrong info

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,11 @@
 class Location < ActiveRecord::Base
 
   validates :location, presence: true
-  validates :address, presence: true
+ 
+  VALID_EMAIL_REGEX=/\b(\d{2,5}\s+)?(([a-zA-Z|\s+]{1,30}){1,4})(court|ct|street|st|drive|dr|lane|ln|road|rd|blvd)/i
+  validates :address, presence: true,format: {with: VALID_EMAIL_REGEX}
+
 	geocoded_by :address
-	after_validation :geocode
+	after_validation :geocode, :if => :address_changed?
 
 end
--- spec/features/request_location_spec.rb
+++ spec/features/request_location_spec.rb
@@ -8,10 +8,8 @@ RSpec.feature "Request Help" do
     click_link "Request a help"
 
     fill_in "Location", with: "King Saud University"
-    fill_in "Address", with: "Riyadh, Riyadh Province"
+    fill_in "Address", with: "12 King Abdullah st"
     fill_in "Description", with: "change car tyre"
-    fill_in "Latitude", with: ""
-    fill_in "Longitude", with: ""
 
     click_button "Create Location"
 
@@ -33,6 +31,23 @@ RSpec.feature "Request Help" do
     expect(page).to have_content("Location has not been created")
     expect(page).to have_content("Location can't be blank")
     expect(page).to have_content("Address can't be blank")
+
+  end
+
+  scenario "A help seeker fill in wrong info" do
+     visit "/" #Root
+
+    click_link "Request a help"
+
+    fill_in "Location", with: "blah"
+    fill_in "Address", with: "blah"
+    fill_in "Description", with: "blah"
+
+
+
+    click_button "Create Location"
+
+    expect(page).to have_content("Location has not been created")
 
   end
 
